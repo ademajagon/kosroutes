7f5025a2a6642e2b96ce96f4c23adafae64fcb93
ademajagon <codebyagon@gmail.com>
2024-05-07 14:18:34 +0200

    feat: add the route location on slug page

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -61,6 +61,37 @@ function RoutePage({
             <h1 className="px-5 py-3 mb-0 -mx-5 text-3xl font-bold text-center -top-5 text-primary">
               {name}
             </h1>
+
+            {route.location && (
+              <div className="flex items-center justify-center">
+                <div className="flex items-center justify-center text-secondary">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    className="w-[14px] mr-1.5 -mt-1"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+
+                  <span className="text-xs font-semibold tracking-wide uppercase">
+                    {route.location}
+                  </span>
+                </div>
+              </div>
+            )}
           </header>
 
           {!isSmall && (
